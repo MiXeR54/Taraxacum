25af865b0fa2dcdc4757bee551485a16349b90d1
MiXeR54 <max_kray@list.ru>
2020-05-30 08:50:04 +0300

    initial commit + some backend changes

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+app.use(express.json());
+
+//DB Connection
+mongoose.connect(config.get("uri"), {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+//Ping DB
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("База данных готова к использованию");
+});
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({}, { collection: "Users" });
+const Users = mongoose.model("User", userSchema);
+
+app.get("/", async (req, res) => {
+  Users.find().then((Users) => res.json(Users));
+});
+
+app.listen(config.get("PORT"), () =>
+  console.log("Server started on " + config.get("PORT"))
+);
